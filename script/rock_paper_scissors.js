73baa24c95582ae6b42cd37375ed9afd123e7bb0
gayadamohammad-coder <gayadamohammad7@gmail.com>
2026-07-28 23:54:57 +0300

    feat: add Rock Paper Scissors UI

--- script/rock_paper_scissors.js
+++ script/rock_paper_scissors.js
@@ -1,73 +1,106 @@
-function playGame(n){
-    function getHumanChoice(){
+const rock = document.createElement("button");
+const paper =document.createElement("button");
+const scissors = document.createElement("button");
 
-        let human_choice = prompt("Enter your choice: ");
+const body = document.querySelector("body");
+body.appendChild(rock);
+body.appendChild(paper);
+body.appendChild(scissors);
 
-        return human_choice;
-        }
-        let human_score=0;
-        let computer_score=0;
+rock.textContent = "Rock";
+paper.textContent = "Paper";
+scissors.textContent = "Scissors";
+
+rock.addEventListener("click",()=>{
+    playRound("rock",getComputerChoice());
+});
+paper.addEventListener("click",()=>{
+    playRound("paper",getComputerChoice())
+});
+scissors.addEventListener("click",()=>{
+    playRound("scissors",getComputerChoice());
+});
+
+const div = document.createElement("div");
+body.appendChild(div);
+
+const result = document.createElement("div");
+body.appendChild(result);
+
+const winner_or_loser = document.createElement("div");
+body.appendChild(winner_or_loser);
+
+const reloading = document.createElement("div");
+body.appendChild(reloading);
+let human_score = 0;
+let computer_score = 0;
+let message = "";
+
  
     function getComputerChoice(){
 
         let  computer_choice= Math.floor(Math.random()*3);
         if(computer_choice===0){
-            return "Scissors";
+            return "scissors";
         }else if (computer_choice===1){
-            return "Paper";
+            return "paper";
         }else if (computer_choice===2){
-            return "Rock";
+            return "rock";
         }
     
         }
-        let round_count=0;
         
-        function playRound(human_choice,computer_choice){
-             
-         human_choice = human_choice.toLowerCase();
-
-        computer_choice= computer_choice.toLowerCase();
+    function playRound(human_choice,computer_choice){
+        
 
          if (human_choice===computer_choice){
-                console.log("Draw");
-        }
-        else if  (human_choice==="scissors" && computer_choice=== "rock"){
-            computer_score +=1;
-            console.log("You lose rock beats scissors");
-        }else if (human_choice==="rock"&& computer_choice==="scissors"){
+                message = "Draw";
+                
+        }else if  (human_choice==="scissors" && computer_choice=== "rock"){
+            message = "You lose rock beats scissors";
+            computer_score+=1;
+        
+        }else if (computer_choice==="scissors"&& human_choice==="rock"){
+            message = "You win rock beats scissors";
             human_score +=1;
-            console.log("You win rock beats scissors");
-        }
-        else if  (human_choice === "paper"&& computer_choice==="rock"){
-            human_score+=1;
-            console.log("You win paper beats rock ");
         }else if (computer_choice=== "paper" && human_choice==="rock"){
+            message = "You lose paper beats rock";
             computer_score+=1;
-            console.log("You lose paper beats rock");
-        }
-        else if (human_choice==="scissors"&& computer_choice==="paper"){
+        }else if(computer_choice==="rock"&& human_choice==="paper"){
+            message= "You win paper beats rock"; 
+            human_score +=1;           
+        }else if(human_choice==="scissors"&& computer_choice==="paper"){
+         
+            message = "You win scissors beat paper ";
             human_score+=1;
-            console.log("You win scissors beat paper ");
         } else if(computer_choice==="scissors"&& human_choice==="paper"){
+            message = "You lose scissors beats paper";
             computer_score+=1;
-            console.log("You lose scissors beats paper");
         }
-        let total_count="Your score: "+" "+human_score+" "+"computer score: "+computer_score;
-            console.log(total_count); 
-}
-        while (round_count<n){
-           let human_choice = getHumanChoice();
-           let computer_choice = getComputerChoice();
-           playRound(human_choice, computer_choice);
-            round_count+=1;
-}
-        if (computer_score>human_score){
-            console.log("computer win");
-        }else if(human_score>computer_score) {
-            console.log("You win");
-        }else{
-            console.log("draw");
+
+        if (computer_score === 5){
+            winner_or_loser.textContent = "Computer Win";
+        }else if(human_score === 5){
+            winner_or_loser.textContent = "Human Win";
         }
-}
-playGame(5);
+         if(computer_score === 5 || human_score === 5){
+            reloading.textContent = "Reloading....";
+            setTimeout(() => {
+            window.location.reload();
+            }, 3000);
+         }
+        
+        let result_message = "Computer score: "+computer_score+" Human score: "+human_score;
+        div.textContent = message;
+        result.textContent= result_message;
+    
+    }
+
+        
+
+    
+        
+
+
+
 
